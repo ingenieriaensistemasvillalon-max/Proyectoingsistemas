633dbfd0d4bec1d4f7c768646e49b03cffa8ae8a
ingenieriaensistemasvillalon-max <ingenieriaensistemasvillalon@gmail.com>
2025-09-25 19:58:39 -0600

    Update script.js
    Signed-off-by: ingenieriaensistemasvillalon-max <ingenieriaensistemasvillalon@gmail.com>

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Declaramos un arreglo (array) con varias canciones de piano
-const Canciones de piano = [
+const Hora de saber musica = [
   "Can't Help Falling in Love.",
   "Nunca es tarde para aprender algo nuevo.",
   "El futuro pertenece a quienes creen en la belleza de sus sueños.",
@@ -21,6 +21,7 @@ function generarCancion() {
   // Cambiamos su contenido de texto por la frase que corresponde al índice generado
   document.getElementById("quote").innerText = Canciones de piano[indice];
 }
+
 
 
 
